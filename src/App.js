61f0dca6fc98b758746e5f73eb70144df05a070c
Abolfazl <abolfazlm181920@gmail.com>
2021-12-19 22:37:26 +0330

    add question option

--- src/App.js
+++ src/App.js
@@ -53,10 +53,7 @@ export default function App() {
 						<div className='question-text'>{questions[0].questionText}</div>
 					</div>
 					<div className='answer-section'>
-						<button>Answer 1</button>
-						<button>Answer 2</button>
-						<button>Answer 3</button>
-						<button>Answer 4</button>
+						{questions[0].answerOptions}
 					</div>
 				</>
 			)}
